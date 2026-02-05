45ef20bbc8cf221adf20a080f6b17d3b4f601555
Akmal Akbarzoda <akbrzda@icloud.com>
2026-02-05 23:44:56 +0500

    Незначительные изменения и обновленный readme.

--- admin-panel/src/shared/components/ui/field/FieldGroup.vue
+++ admin-panel/src/shared/components/ui/field/FieldGroup.vue
@@ -7,15 +7,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div
-    data-slot="field-group"
-    :class="
-      cn(
-        'group/field-group @container/field-group flex w-full flex-col gap-7 data-[slot=checkbox-group]:gap-3 [&>[data-slot=field-group]]:gap-4',
-        props.class,
-      )
-    "
-  >
+  <div data-slot="field-group" :class="cn('group/field-group @container/field-group flex w-full flex-col gap-3', props.class)">
     <slot />
   </div>
 </template>
